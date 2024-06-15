928414c6997b737ced899be2f61c28e60f0be55e
Kirill Zubovsky <kz@kirillzubovsky.com>
2024-06-15 13:55:18 -0700

    wip open AI

--- src/app/gpt/route.js
+++ src/app/gpt/route.js
@@ -18,17 +18,17 @@ export async function POST(req) {
         "messages": [
               {
                 "role": "system",
-                "content": "You are a manager for a firm that writes post cards to house owners, selling "
+                "content": "You are a manager for a firm that writes post cards to house owners, selling them house paint jobs that make the house look better and more expensive. You will get a description of the house current paint job status, and your job is to write a one sentence message that will entice the house owner to sign up for our service to pain their house."
               },
               {
                 "role": "user",
-                "content": `${userContentRequest}`
+                "content": `${moonresponse}`
               },
             ],
       },
       {
         headers: {
-          'Authorization': `Bearer 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`,
+          'Authorization': `Bearer ${process.env.OPENAI}`,
           'Content-Type': 'application/json',
         },
       }
@@ -38,13 +38,11 @@ export async function POST(req) {
     const generatedText = response.data.choices[0].message.content;
     // console.log(response.data.choices[0]);
 
-    getDataAndStoreToMongoDB(generatedText, complexity)    
-
   } catch (error) {
     console.error('Error calling OpenAI API:', error);
     return null; // Handle errors appropriately in your application
   }
   
-  return NextResponse.json({ message: "Success!"}, {status: 200});
+  return NextResponse.json({ message: generatedText}, {status: 200});
 
 }
